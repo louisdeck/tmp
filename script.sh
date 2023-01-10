2fbb46fba5e6e3b8f9d831894a02d046c100a197
louisdeck <louiisd@protonmail.com>
2023-01-10 17:17:57 +0100

    hein hein

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 10.128.10.100/12
+# 10.16.0.0/16
+
+echo "Starting recon."
+
+for i in {0..255}
+do
+    for j in {0..255}
+    do
+        #echo "10.128.$i.$j"
+        nmap -P0 -e eth1 -S 10.128.$i.$j 10.16.$i.$j
+    done
+done
+
+echo "Ending recon."
