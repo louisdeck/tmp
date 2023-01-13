a9443c2b737e3ceac1a541febfeb3237fd3ba96c
louisdeck <louiisd@protonmail.com>
2023-01-13 10:54:38 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ echo "Starting recon."
 
 for i in {1..255}
 do
-    for j in {1..255}
+    for j in {0..255}
     do
-        nmap -sn --max-retries 0 -e eth1 -S 10.128.$i.$j 10.16.$i.$j
+        nmap -sn -e eth1 -S 10.128.$i.$j 10.16.$i.$j
     done
 done
 
