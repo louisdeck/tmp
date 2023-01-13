242991da33d0487a9a4cb8bb5228f75c543390ad
louisdeck <louiisd@protonmail.com>
2023-01-13 17:01:17 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 echo "Starting recon."
 
-for i in {1..255}
+for i in {0..255}
 do
     for j in {0..255}
     do
-        nmap -sn -e eth1 -S 10.128.$i.$j 10.16.$i.$j
+        nmap -T1 -sn -e eth1 --send-ip -S 10.128.$i.$j 10.16.$i.$j
     done
 done
 
